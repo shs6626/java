1bc27e65ccaf884a61d60a95537e2aa53f534e73
hongunner <shs6626@naver.com>
2024-01-17 08:00:45 +0900

    Operator

---
+++ java_basic/src/Operator.java
@@ -0,0 +1,10 @@
+public class Operator {
+  public static void main(String[] args) {
+    int num = 20;
+    int num2 = 30;
+    String str = "a+b = ";
+    String aa = str + num + num2;
+    System.out.println(aa);
+
+  }
+}
